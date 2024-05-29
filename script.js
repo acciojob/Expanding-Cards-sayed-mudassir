3dcd63ffe350fcd245fd94941c89586bf7e3d329
sayed mudassir <sayedmuddassir68@gmail.com>
2024-05-30 03:33:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const image = document.getElementsByTagName("img");
+function makeAllOtherInActive(active){
+    for(let i = 0; i<image.length;i++){
+        if(i!=active){
+            image[i].classList.remove("active");
+            image[i].classList.add("pannel");
+            image[i].nextElementSibling.classList.add("hidden");
+            image[i].nextElementSibling.classList.remove("img-text");
+        }
+    }  
+}
+
+for(let i = 0; i<image.length;i++){
+    image[i].addEventListener("click",(e)=>{
+       image[i].classList.toggle("pannel");
+       image[i].classList.toggle("active");
+       image[i].nextElementSibling.classList.toggle("hidden");
+       image[i].nextElementSibling.classList.toggle("img-text");
+       makeAllOtherInActive(i);
+    })
+}
